15a795f61bd301ea655d180ff314ca6cf7a62c02
scentmay <scentmay@gmail.com>
2022-06-22 19:45:08 +0000

    Py funcionando a falta del color

--- src/app.js
+++ src/app.js
@@ -5,7 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let valores = ["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"];
+let simbolos = [
+  {
+    icono: "♦",
+    clase: "diamond",
+    color: "black"
+  },
+  {
+    icono: "♥",
+    clase: "heart",
+    color: "red"
+  },
+  {
+    icono: "♠",
+    clase: "spade",
+    color: "red"
+  },
+  {
+    icono: "♣",
+    clase: "club",
+    color: "black"
+  }
+];
+
+// 1.- Generamos un valor random para la carta
+function generarValor() {
+  let indice = Math.floor(Math.random() * 12);
+  return valores[indice];
+}
+
+// 2.- Generamos símbolo aleatorio y devolvemos tanto el símbolo como la clase:
+function generarSimbolo() {
+  let indice = Math.floor(Math.random() * 4);
+  return simbolos[indice].icono;
+}
+
+// 3.-Insertar los valores del objeto al HTML
+function renderCard() {
+  document.getElementById("head").innerHTML = generarSimbolo();
+  document.getElementById("center").innerHTML = generarValor();
+  document.getElementById("footer").innerHTML = document.getElementById(
+    "head"
+  ).innerHTML;
+}
+
+renderCard();
